b3ea5ea75885add99cb8dfa9d3638e931f188b03
bayusatryiabys <74623143+bayusatryiabys@users.noreply.github.com>
2020-11-20 23:58:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = '🤖*BOT_PUTRY'; 
+const BotName = '🤖*BOT_PUTRY🤖'; 
 const instagram = 'https://Instragram.com/kumpulan_foto_anime'; 
 const telegram ='-'; 
 const kapanbotaktif = 'SETIAP HARI'; 
